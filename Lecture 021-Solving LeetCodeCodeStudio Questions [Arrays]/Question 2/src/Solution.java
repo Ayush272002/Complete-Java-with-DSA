3ff4c5b3be19345336d5046c0faf2dbf4ab16c35
Ayush272002 <ayushacharjya@gmail.com>
2023-12-20 12:26:47 +0000

    Added Q2 from Intellij

---
+++ Lecture 021-Solving LeetCodeCodeStudio Questions [Arrays]/Question 2/src/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean check(int[] nums) {
+        int count =0;
+        int n = nums.length;
+        for (int i = 1; i < n; i++) {
+            if(nums[i-1]> nums[i])
+                count++;
+        }
+        if(nums[n-1]>nums[0])
+            count++;
+
+        if(count<=1) return true;
+
+        return false;
+
+    }
+}
